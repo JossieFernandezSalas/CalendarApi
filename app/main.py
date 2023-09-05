a44db40a15f2e477675a15189a916744913f8cff
Jossie Esteban Fernández Salas <jossie.fernandez.salas@gmail.com>
2023-09-05 17:49:38 -0600

    Enabling cors

--- app/main.py
+++ app/main.py
@@ -5,8 +5,25 @@
 from fastapi import FastAPI
 from app.routes import calendar
 from app.services import calendar_services
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 app.include_router(calendar.calendar_routes, prefix="/calendar")
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 calendar_services.get_calendar_service()
